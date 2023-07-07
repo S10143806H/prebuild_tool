1d2b8423f57d6a5eb65d44f3bc79b39e63d49721
Dakamaster <644165318.tj@gmail.com>
2023-07-07 10:21:36 +0800

    Update prebuild_tool.c

--- prebuild_tool/prebuild_tool.c
+++ prebuild_tool/prebuild_tool.c
@@ -2,12 +2,12 @@
 *  README
 *
 * FOR REALTEK INTERNAL USERS
-* To understand the tool overall API structure and details, please download a documentation via the Wiki link below and refer to Chapter 2:
+* To understand more about the tools' API structure and details, please download the documentation via the Wiki link below and refer to Chapter 2:
 * https://wiki.realtek.com/download/attachments/155451805/%5BWiki%5D%5BSD3%5D%20How%20to%20update%20tools%20to%20Arduino%20SDK_20230707.pdf?api=v2
-* 
+*
 *  COMPILATION
-* Windows: VS Build Solution0
-* Linux/macOS: open terminal and execute "make clean;make" command
+* Windows: Open Microsoft Visual Studio, then click the "Build Solution" button.
+* Linux/macOS: Open the terminal and execute the "make clean;make" command
 *
 *******************************************************************************************************************************************************************/
 
